491c96c13ecf7e81ab3e11db6d840c1cc2fc4384
Moon-999 <munhui3078@naver.com>
2022-11-15 14:01:51 +0900

    updated

--- app.py
+++ app.py
@@ -226,6 +226,48 @@ def querySQL2():
     return responseBody
 
 
+## Query 조회3
+@app.route('/api/querySQL3', methods=['POST'])
+def querySQL3():
+    
+    body = request.get_json()
+    print(body, type(body))
+    
+    location04 = body['action']['params']['sys_location04']
+
+    query_str = f'''
+        SELECT DISTINCT "TYPE" FROM apt2 where "NAME" = '{location04}'
+    '''
+    print('---------------')
+    print(query_str)
+    print('---------------')
+
+    engine = create_engine("postgresql://qxqcovcxobgrzr:136d1a4ee21d7d53fefe41723c82cadb3a41edd4203ef9b4759b8ecb1daf68a7@ec2-107-23-76-12.compute-1.amazonaws.com:5432/d7477vdhmjaq31", echo = False)
+
+    with engine.connect() as conn:
+        query = conn.execute(text(query_str))
+
+    df = pd.DataFrame(query.fetchall())
+    print('-----------')
+    print(df)
+    print('----------')
+    results = df['TYPE'].tolist()
+    answer_text = '/ '.join(results)  
+
+    responseBody = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": answer_text
+                    }
+                }
+            ]
+        }
+    }
+    return responseBody
+
 if __name__ == "__main__":
     db_create()
     app.run()
