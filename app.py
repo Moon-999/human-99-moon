83be2bd657fb12b2c4bd68b8c9aaedf5b33620b1
Moon-999 <munhui3078@naver.com>
2022-11-15 11:42:44 +0900

    updated

--- app.py
+++ app.py
@@ -192,7 +192,10 @@ def querySQL2():
     query_str = f'''
         SELECT DISTINCT "NAME" FROM apt2 where "CITY" = '{location01}' and "GU" = '{location02}' and "DONG" = '{location03}'
     '''
-
+    print('---------------')
+    print(query_str)
+    print('---------------')
+    
     engine = create_engine("postgresql://qxqcovcxobgrzr:136d1a4ee21d7d53fefe41723c82cadb3a41edd4203ef9b4759b8ecb1daf68a7@ec2-107-23-76-12.compute-1.amazonaws.com:5432/d7477vdhmjaq31", echo = False)
 
     with engine.connect() as conn:
