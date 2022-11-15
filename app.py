7cd1d29e0e7c35f972e88c18380292fc8ab9a93b
Moon-999 <munhui3078@naver.com>
2022-11-15 15:52:50 +0900

    updated

--- app.py
+++ app.py
@@ -276,7 +276,7 @@ def querySQL4():
     print(body, type(body))
     
     location04 = body['action']['params']['sys_location04']
-    sys_number = int(body['action']['params']['sys_number'])
+    sys_number = body['action']['params']['sys_number']['amount']
     print(sys_number, type(sys_number))
     # params_df = body['action']['params']
     # print(params_df, print(type(params_df)))
