705ec1dbf35ed0c025403005c4d917b635acaa20
Moon-999 <munhui3078@naver.com>
2022-11-15 15:46:32 +0900

    updated

--- app.py
+++ app.py
@@ -276,7 +276,7 @@ def querySQL4():
     print(body, type(body))
     
     location04 = body['action']['params']['sys_location04']
-    sys_number = str(body['action']['params']['sys_number']['amount'])
+    sys_number = body['action']['params']['sys_number']
     # params_df = body['action']['params']
     # print(params_df, print(type(params_df)))
     # sys_number = str(json.loads(params_df['sys_number'])['amount'])
