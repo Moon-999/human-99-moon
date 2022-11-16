acda0b0d577976a823488ba7e8154c5ebc3fe828
Moon-999 <munhui3078@naver.com>
2022-11-16 14:30:36 +0900

    updated

--- app.py
+++ app.py
@@ -350,29 +350,14 @@ def querySQL5():
     # print(params_df, print(type(params_df)))
     # sys_number = str(json.loads(params_df['sys_number'])['amount'])
 
-    if price <= 600000000:
-        responseBody = {
+    responseBody = {
         "version": "2.0",
             "template": {
                 "outputs": [
                     {
                         "simpleText": {
                             "text": "위에서 입력하신 기준으로 고객님은 안심전환대출 신청이 가능합니다. \n (유의사항) 주택의 시세는 변동될 수 있으며, 최종 대출 가능 여부는 실제 대출심사를 통해 확인할 수 있습니다."
-                         }
-                        }
-                    ]
-                }
-     }
-        return responseBody
-    elif price > 600000000:
-       responseBody = {
-        "version": "2.0",
-            "template": {
-                "outputs": [
-                    {
-                        "simpleText": {
-                            "text": "주택가격이 신청일 기준 6억원을 초과할 경우 안심전환대출신청이 불가합니다."
-                         }
+                         } if price <= 600000000 else "주택가격이 신청일 기준 6억원을 초과할 경우 안심전환대출신청이 불가합니다."
                         }
                     ]
                 }
