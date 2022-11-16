b3ffc9164723acd9628090766c95496390fa22ca
Moon-999 <munhui3078@naver.com>
2022-11-16 10:20:09 +0900

    updated

--- app.py
+++ app.py
@@ -263,7 +263,7 @@ def querySQL3():
             "outputs": [
                 {
                     "simpleText": {
-                        "text": location04 + "(을)를 선택하셨습니다. \n 해당 아파트의 타입은 \n [" + answer_text + "] \n 입니다 \n 해당하시는 아파트의 타입을 입력해주세요."
+                        "text": location04 + "(을)를 선택하셨습니다. \n 해당 아파트의 타입은 \n [" + answer_text + "] \t 입니다. \n 해당하시는 아파트의 타입을 입력해주세요."
                     }
                 }
             ]
@@ -317,7 +317,7 @@ def querySQL4():
             "outputs": [
                 {
                     "simpleText": {
-                        "text": answer_text
+                        "text": answer_text + "원 입니다."
                     }
                 }
             ]
